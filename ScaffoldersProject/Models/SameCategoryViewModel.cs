210fae31721dac752a76925d4d40e2d999cc47a6
Teo <teo4asoee@gmail.com>
2018-01-11 13:32:56 +0200

    add View viewProduct and SameProductViewModel

---
+++ ScaffoldersProject/Models/SameCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScaffoldersProject.Models
+{
+    public class SameCategoryViewModel
+    {
+        public Products Product { get; set; }
+        public List<Products> SimilarProducts { get; set; }
+
+        public SameCategoryViewModel(Products p , List<Products> similarProducts)
+        {
+            Product = p;
+            SimilarProducts = similarProducts;
+        }
+    }
+}
