6a81a17f2dbc47d45256b70e4924f77420fb2a25
Teo <teo4asoee@gmail.com>
2018-01-11 09:42:56 +0200

    Added clientController and viewProduct

---
+++ ScaffoldersProject/Controllers/ClientController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScaffoldersProject.Controllers
+{
+    public class ClientController : Controller
+    {
+        //private IProductRepository _repository;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Θα καλειται οταν ο client κλικαρει σε ενα προιον και θα του το εμφανιζει μονο του με τα details
+        public IActionResult ViewProduct(int productId)
+        {
+            return View(/*_repository.Products.FirstOrDefault(p => p.ProductID == productId)*/);
+        }
+
+        public IActionResult AddToCart(int productId)
+        {
+
+        }
+
+    }
+}
--- ScaffoldersProject/Data/Migrations/20180110194432_initial.Designer.cs
+++ ScaffoldersProject/Data/Migrations/20180110194432_initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ScaffoldersProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180110194432_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
