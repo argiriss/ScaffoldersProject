664559483cce9b41cb50df75ca6b3713560be4ed
stadio18 <stadio18@hotmail.com>
2018-01-22 22:18:03 +0200

    Deposit migration

---
+++ ScaffoldersProject/Data/Migrations/20180122192312_DepositCreate.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ScaffoldersProject.Data.Migrations
+{
+    public partial class DepositCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItem",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "CartItem",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Deposit",
+                columns: table => new
+                {
+                    DepositId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DepositAmount = table.Column<decimal>(nullable: false),
+                    DepositDate = table.Column<DateTime>(nullable: false),
+                    UserDepositId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deposit", x => x.DepositId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem",
+                column: "CartId",
+                principalTable: "Cart",
+                principalColumn: "CartId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem");
+
+            migrationBuilder.DropTable(
+                name: "Deposit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItem",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "CartItem",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Cart_CartId",
+                table: "CartItem",
+                column: "CartId",
+                principalTable: "Cart",
+                principalColumn: "CartId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItem_Products_ProductId",
+                table: "CartItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
