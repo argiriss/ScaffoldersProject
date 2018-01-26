509a849fc6559bc2b6a2f4b44b4bfe86854080d1
Teo <teo4asoee@gmail.com>
2018-01-26 09:59:46 +0200

    form Paypal

---
+++ ScaffoldersProject/Models/services/EfWalletRepository.cs
@@ -0,0 +1,27 @@
+﻿using ScaffoldersProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScaffoldersProject.Models.services
+{
+    public class EfWalletRepository
+    {
+        private MainDbContext db;
+        public IQueryable<Deposit> Orders => db.Deposit;
+
+        public EfWalletRepository(MainDbContext db)
+        {
+            this.db = db;
+        }
+        //Deposit to my wallet via Paypal
+        public void Deposit(decimal Amount , string UserId)
+        {            
+        }
+
+        //pay from my wallet
+        public void PayFromWallet(decimal Amount , string UserId) { }
+
+    }
+}
