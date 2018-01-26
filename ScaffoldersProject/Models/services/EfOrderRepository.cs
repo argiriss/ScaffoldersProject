289096ebb60d243dd4c11de82e2e7e91459f5b0d
stadio18 <stadio18@hotmail.com>
2018-01-26 09:25:52 +0200

    orderChange

--- ScaffoldersProject/Models/services/EfOrderRepository.cs
+++ ScaffoldersProject/Models/services/EfOrderRepository.cs
@@ -18,33 +18,36 @@ namespace ScaffoldersProject.Models.services
         {
             this.db = db;
         }
+
         //method for Adding the new order to Order table
         public void AddNewOrder(Order orderDetails , Cart c)
         {
-        //    //Add the order to Order Table
-        //    db.Order.Add(orderDetails);
-        //    db.SaveChanges();
-        //    //Find the cartitems which exist in User's Cart
-        //    var CartItemsOrdered = db.CartItem.Where(x => x.CartId == c.CartId);
-        //    //for those CartItems set the Orderid(fk) to marked as ordered
-        //    foreach (var item in CartItemsOrdered)
-        //    {
-        //        item.OrderID = orderDetails.OrderID;
-        //        //for each product in product table which productId exist in CartItemsOrdered list reduce the stock
-        //        foreach (var prod in db.Products)
-        //        {
-        //            if (prod.ProductId==item.ProductId)
-        //            {
-        //                prod.Stock -= item.Quantity; //reduce the stock
-        //                db.Products.Update(prod); //update the table
-        //            }
-        //        }
-                
-        //    }
-        //    db.CartItem.UpdateRange(CartItemsOrdered);
-        //    //Update the Product table by reducing the stock of ordered product respectively
-            
-        //    db.SaveChanges();
+            //Add the order to Order Table and save it
+          
+            db.Order.Add(orderDetails);
+            db.SaveChanges();
+
+            //Find the cartitems which exist in User's Cart
+            var CartItemsOrdered = db.Cart.Where(x => x.UserCartId == c.UserCartId);
+            //for those CartItems set the Orderid(fk) to marked as ordered
+            foreach (var item in CartItemsOrdered)
+            {
+                item.OrderID = orderDetails.OrderID;
+                //for each product in product table which productId exist in CartItemsOrdered list reduce the stock
+                foreach (var prod in db.Products)
+                {
+                    if (prod.ProductId == item.ProductId)
+                    {
+                        prod.Stock -= item.Quantity; //reduce the stock
+                        db.Products.Update(prod); //update the table
+                    }
+                }
+
+            }
+            db.CartItem.UpdateRange(CartItemsOrdered);
+            //Update the Product table by reducing the stock of ordered product respectively
+
+            db.SaveChanges();
         }
     }
 }
