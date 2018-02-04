fbf5e40a75ab6623a0f282267ab9d91faffdd202
stadio18 <stadio18@hotmail.com>
2018-02-04 18:10:02 +0200

    Bid

---
+++ ScaffoldersProject/Data/Migrations/20180204150431_askOfferDecimal.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ScaffoldersProject.Data.Migrations
+{
+    public partial class askOfferDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                table: "Offer",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                table: "Ask",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Offer",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Ask",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
